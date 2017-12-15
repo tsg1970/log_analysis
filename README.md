a4786eda086a0d2c5c91c021b39330b7b30ddd2e
Tim Goff <tsgoff@att.net>
2017-12-14 22:22:17 -0600

    Made changes recommended by first review of project:

--- README.md
+++ README.md
@@ -5,15 +5,17 @@ Python Log Analysis
 website: https://github.com/tsg1970/log_analysis.git
 email: tsgoff@att.net
 
-## To view website run the entertainment_center.py file
-
 # Project Overview
 
 To create reporting tool that prints out reports based on the data in a
 PostGresql database. The reporting tool is a Python program using the psycopg2
 module to connect to the database.
 
-To access the reporting tool type newsdatadb.py at a command prompt.
+The database can be downloaded at the following link: https://d17h27t6h515a5.cloudfront.net/topher/2016/August/57b5f748_newsdata/newsdata.zip
+Unzip the file into the same directory as a virtual machine.
+
+The file used to access the database is newsdatadb.py. Save file in the same directory
+as the database. To access the reporting tool type newsdatadb.py at a command prompt.
 
 The code answers 3 questions:
   What are the most popular three articles of all time?
--- newsdatadb.py
+++ newsdatadb.py
@@ -1,55 +1,78 @@
+#! /usr/bin/env python3
+
 # Python code for the Udacity Log Analysis project
 
 import psycopg2
 import pprint
 
-# Create variable for the news database
-dbname = "news"
+
+def connect(database_name="news"):
+    try:
+        db = psycopg2.connect("dbname={}".format(database_name))
+        cursor = db.cursor()
+        return db, cursor
+    except:
+        print("There was an error accessing the database")
 
 
-def main():
+def poparticles():
     """Retrieve data for the most popular 3 articles of all time"""
-    db = psycopg2.connect(database=dbname)
-    c = db.cursor()
-
-    c.execute("SELECT articles.title, count(*) AS num FROM articles, log WHERE log.path \
-            LIKE '%'||articles.slug||'%' \
-            GROUP BY articles.title ORDER BY count(*) DESC LIMIT 3")
-    records = c.fetchall()
+    db, cursor = connect()
+    query = "SELECT title, count(*) AS views \
+        FROM log join articles \
+        on log.path = concat('/article/', articles.slug) \
+        group by title \
+        order by views DESC \
+        limit 3"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    db.close()
     print("\n" + "The most popular three articles of all time:")
     for record in records:
         print "\t" + str(record[0]) + " -- " + str(record[1]) + " views"
 
+
+def popauthors():
     """Retrieve data for the most popular article authors of all time."""
-    c.execute("SELECT authors.name, count(*) AS num FROM articles, log, authors \
-            WHERE authors.id = articles.author and \
-            log.path LIKE '%'||articles.slug||'%' \
-            GROUP BY authors.name ORDER BY count(*) DESC")
-    records = c.fetchall()
+    db, cursor = connect()
+    query = "SELECT name, count(*) AS views \
+        FROM log join articles \
+        on log.path = concat('/article/', articles.slug) \
+        join authors \
+        ON authors.id =  articles.author \
+        group by authors.name \
+        order by views DESC"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    db.close()
     print("\n" + "The most popular article authors of all time:")
     for record in records:
         print "\t" + str(record[0]) + " -- " + str(record[1]) + " views"
 
-    """Retrieve data for days of more than 1 pct of requests lead to errors"""
-    c.execute("WITH c1_table AS( \
-            SELECT to_char(time,'Month DD, YYYY') AS errdate, \
-            count((status != '200 OK') OR NULL) AS errcnt, \
-            count(*) As total \
-            FROM log \
-            GROUP BY to_char(time, 'Month DD, YYYY') \
-            ORDER BY to_char(time, 'Month DD, YYYY') DESC \
-            ), c2_table AS( \
-            SELECT errdate, round((errcnt * 100.0) / total, 2) AS errpct \
-            FROM c1_table\
-            ) \
-            SELECT errdate, errpct FROM c2_table WHERE errpct > 1 \
-            ")
 
-    records = c.fetchall()
+def requesterr():
+    """Retrieve data for days of more than 1 pct of requests lead to errors"""
+    db, cursor = connect()
+    query = "SELECT to_char(date, 'FMMonth FMDD, YYYY'),\
+        errcnt/total* 100 AS ratio\
+        FROM (SELECT time::date AS date,\
+        count(*) AS total,\
+        sum((status != '200 OK')::int)::float AS errcnt\
+        FROM log\
+        GROUP BY date) AS errors\
+        WHERE errcnt/total > .01"
+    cursor.execute(query)
+    records = cursor.fetchall()
+    db.close()
     print("\n" + "Days with more than 1% of requests lead to errors:")
     for record in records:
         print "\t" + str(record[0]) + " -- " + str(record[1]) + "%"
 
+
+def main():
+    poparticles()
+    popauthors()
+    requesterr()
 
 if __name__ == '__main__':
     main()
